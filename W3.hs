e7ca5d7b7af8b3e5e1d8bdbd99db7258cdbb675b
Joel Kaasinen <joel.kaasinen@gmail.com>
2018-03-28 14:09:28 +0300

    W3 exercises

---
+++ W3.hs
@@ -0,0 +1,377 @@
+module W3 where
+
+-- Week 3:
+--   * built-in datatypes
+--   * custom datatypes
+--   * folds
+--
+-- Useful functions / types:
+--   * Maybe
+--
+-- Don't add any new imports!
+
+------------------------------------------------------------------------------
+-- Ex 1: implement safe integer devision, that is, a function that
+-- returns Just result normally, but Nothing if the divisor is zero.
+--
+-- Remember that integer division can be done with the div function.
+
+safeDiv :: Integer -> Integer -> Maybe Integer
+safeDiv x y = undefined
+
+------------------------------------------------------------------------------
+-- Ex 2: another variant of safe division. This time a succesful
+-- division should be returned as
+--   Right result
+-- whereas a division by zero should return
+--   Left "1234/0"
+-- (replace 1234 with the value of x).
+
+eitherDiv :: Integer -> Integer -> Either String Integer
+eitherDiv x y = undefined
+
+------------------------------------------------------------------------------
+-- Ex 3: implement the function addEithers, which combines two values of type
+-- Either String Int into one like this:
+--
+-- - If both inputs were Ints, sum the Ints
+-- - Otherwise, return the first argument that was not an Int
+--
+-- Exmaples:
+--
+-- addEithers (Right 1) (Right 2) ==> Right 3
+-- addEithers (Right 1) (Left "fail") ==> Left "fail"
+-- addEithers (Left "boom") (Left "fail") ==> Left "boom"
+
+addEithers :: Either String Int -> Either String Int -> Either String Int
+addEithers a b = undefined
+
+------------------------------------------------------------------------------
+-- Ex 4: implement the function mapMaybe, which works a bit like a
+-- combined map & filter.
+---
+-- mapMaybe is given a list ([a]) and a function of type a -> Maybe b.
+-- This function is called for all values in the list. If the function
+-- returns Just x, x will be in the result list. If the function
+-- returns Nothing, nothing gets added to the result list.
+--
+-- Examples:
+--
+-- let f x = if x>0 then Just (2*x) else Nothing
+-- in mapMaybe f [0,1,-1,4,-2,2]
+--   ==> [2,8,4]
+--
+-- mapMaybe Just [1,2,3]
+--   ==> [1,2,3]
+--
+-- mapMaybe (\x -> Nothing) [1,2,3]
+--   ==> []
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+mapMaybe f xs = undefined
+
+------------------------------------------------------------------------------
+-- Ex 5: define a datatype Person, which should contain the age (an
+-- Int) and the name (a String) of a person.
+--
+-- Also define a Person value fred, and the functions getAge, getname,
+-- setAge and setName (see below).
+
+data Person = PersonUndefined
+  deriving Show
+
+-- fred is a person whose name is Fred and age is 90
+fred :: Person
+fred = undefined
+
+-- getName returns the name of the person
+getName :: Person -> String
+getName p = undefined
+
+-- getAge returns the age of the person
+getAge :: Person -> Int
+getAge p = undefined
+
+-- setName takes a person and returns a new person with the name changed
+setName :: String -> Person -> Person
+setName name p = undefined
+
+-- setAge does likewise for age
+setAge :: Int -> Person -> Person
+setAge age p = undefined
+
+------------------------------------------------------------------------------
+-- Ex 6&7: define a datatype TwoCounters which contains two Int
+-- counters. Also define the functions below for operating a
+-- TwoCounters.
+--
+-- This exercise is worth two points.
+--
+-- Examples:
+--
+-- getA (incA (incA zeros))
+--   ==> 2
+-- getB (incB (incA zeros))
+--   ==> 1
+
+data TwoCounters = Undefined
+
+-- zeros is a TwoCounters value with both counters initialized to 0
+zeros :: TwoCounters
+zeros = undefined
+
+-- getA returns the value of the "A" counter
+getA :: TwoCounters -> Int
+getA tc = undefined
+
+-- getB returns the value of the "B" counter
+getB :: TwoCounters -> Int
+getB tc = undefined
+
+-- incA increases the value of the "A" counter by one
+incA :: TwoCounters -> TwoCounters
+incA tc = undefined
+
+-- incB does likewise for the "B" counter
+incB :: TwoCounters -> TwoCounters
+incB tc = undefined
+
+------------------------------------------------------------------------------
+-- Ex 8&9: define a datatype UpDown that represents a counter that can
+-- either be in incresing or decreasing mode. Also implement the
+-- functions zero, toggle, tick and get below.
+--
+-- NB! Define _two_ constructors for your datatype, in other words,
+-- the definition should be of the form
+--   data UpDown = A something | B foobar
+--
+-- This exercise is worth two points
+--
+-- Examples:
+--
+-- get (tick zero)
+--   ==> 1
+-- get (tick (tick zero))
+--   ==> 2
+-- get (tick (tick (toggle (tick zero))))
+--   ==> -1
+
+data UpDown = UpDownUndefined1 | UpDownUndefined2
+
+-- zero is an increasing counter with value 0
+zero :: UpDown
+zero = undefined
+
+-- get returns the counter value
+get :: UpDown -> Int
+get ud = undefined
+
+-- tick increases an increasing counter by one or decreases a
+-- decreasing counter by one
+tick :: UpDown -> UpDown
+tick ud = undefined
+
+-- toggle changes an increasing counter into a decreasing counter and
+-- vice versa
+toggle :: UpDown -> UpDown
+toggle ud = undefined
+
+--------------------------------------------------------------------------------
+-- The next exercises use the binary tree type defined like this:
+
+data Tree a = Leaf | Node a (Tree a) (Tree a)
+            deriving (Show, Eq)
+
+-- Ex 10: implement the function valAtRoot which returns the value at
+-- the root (top-most node) of the tree. The return value is Maybe a
+-- because the tree might be empty (i.e. just a Leaf)
+
+valAtRoot :: Tree a -> Maybe a
+valAtRoot t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 11: compute the size of a tree, that is, the number of Node
+-- constructors in it
+
+treeSize :: Tree a -> Int
+treeSize t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 12: get the leftmost value in the tree. The return value is
+-- Maybe a because the tree might be empty.
+--
+-- The leftmost value means the value that is reached by going to the
+-- left child of the current Node for as long as possible.
+--
+-- Examples:
+--
+-- leftest Leaf
+--   ==> Nothing
+-- leftest (Node 1 (Node 2 (Node 3 Leaf Leaf) Leaf) Leaf)
+--   ==> Just 3
+-- leftest (Node 1 (Node 2 Leaf (Node 3 Leaf Leaf)) (Node 4 Leaf Leaf))
+--   ==> Just 2
+
+leftest :: Tree a -> Maybe a
+leftest t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 13: implement map for trees.
+--
+-- Examples:
+--
+-- mapTree (+1) Leaf  ==>  Leaf
+-- mapTree (+2) (Node 0 (Node 1 Leaf Leaf) (Node 2 Leaf Leaf))
+--   ==> (Node 2 (Node 3 Leaf Leaf) (Node 4 Leaf Leaf))
+
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 14: insert the given value into the leftmost possible place. You
+-- need to return a new tree since the function is pure.
+--
+-- Example:
+-- insertL 0 Leaf
+--   ==> Node 0 Leaf Leaf
+-- insertL 0 (Node 1 Leaf Leaf)
+--   ==> Node 1 (Node 0 Leaf Leaf) Leaf)
+--
+-- insertL 0 (Node 1
+--             (Node 2
+--               Leaf
+--               (Node 3 Leaf Leaf))
+--             (Node 4 Leaf Leaf))
+--
+--        ==> Node 1
+--             (Node 2
+--               (Node 0 Leaf Leaf)
+--               (Node 3 Leaf Leaf))
+--             (Node 4 Leaf Leaf)
+
+
+insertL :: a -> Tree a -> Tree a
+insertL x t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 15: implement the function measure, that takes a tree and
+-- returns a tree with the same shape, but with the value at every
+-- node being the size of the subtree starting at that node.
+--
+-- Tree size is defined as in Ex 9
+--
+-- Examples:
+--
+-- measure (Node 'a' Leaf Leaf)
+--  ==> Node 1 Leaf Leaf
+-- measure (Node 'a' (Node 'b' Leaf Leaf) Leaf)
+--  ==> Node 2 (Node 1 Leaf Leaf) Leaf
+-- measure (Node 0 (Node 0 Leaf Leaf) Leaf)
+--  ==> Node 2 (Node 1 Leaf Leaf) Leaf
+-- measure (Node 0 (Node 0 Leaf Leaf)
+--                 (Node 0 (Node 0 Leaf Leaf)
+--                         (Node 0 Leaf
+--                                 (Node 0 Leaf Leaf))))
+--      ==> Node 6 (Node 1 Leaf Leaf)
+--                 (Node 4 (Node 1 Leaf Leaf)
+--                         (Node 2 Leaf
+--                                 (Node 1 Leaf Leaf)))
+
+
+measure :: Tree a -> Tree Int
+measure t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 16: the standard library function
+--   foldr :: (a -> b -> b) -> b -> [a] -> b
+-- is used to "collapse" a list to a single value, like this:
+--   foldr f start [x,y,z,w]
+--     ==> f x (f y (f z (f w start)
+--
+-- Implement the functions sumf and lengthf so that mysum computes the
+-- sum of the values in the list and mylength computes the length of
+-- the list.
+--
+-- DON'T change the definitions of mysum and mylength, only implement
+-- sumf and lengtf appropriately.
+
+mysum :: [Int] -> Int
+mysum is = foldr sumf 0 is
+
+sumf :: Int -> Int -> Int
+sumf x y = undefined
+
+mylength :: [a] -> Int
+mylength xs = foldr lengthf 0 xs
+
+lengthf :: a -> Int -> Int
+lengthf x y = undefined
+
+------------------------------------------------------------------------------
+-- Ex 17: implement the function foldTree that works like foldr, but
+-- for Trees.
+--
+-- Example:
+--   foldTree f l (Node 3 Leaf Leaf)
+--     ==> f 3 l l
+--   foldTree f l (Node 'a' (Node 'b' (Node 'c' Leaf Leaf)
+--                                    Leaf)
+--                          (Node 'd' Leaf Leaf))
+--     ==> f (f 'a' (f 'b' (f 'c' l l)
+--                         l)
+--                  (f 'd' l l))
+--
+-- Once you've implemented foldTree correctly, the functions treeSum
+-- and treeLeaves below work correctly.
+
+sumt :: Int -> Int -> Int -> Int
+sumt x y z = x+y+z
+
+-- Sum of numbers in the tree
+treeSum :: Tree Int -> Int
+treeSum t = foldTree sumt 0 t
+
+leaft :: a -> Int -> Int -> Int
+leaft x y z = y+z
+
+-- Number of leaves in the tree
+treeLeaves :: Tree a -> Int
+treeLeaves t = foldTree leaft 1 t
+
+foldTree :: (a -> b -> b -> b) -> b -> Tree a -> b
+foldTree f x t = undefined
+
+------------------------------------------------------------------------------
+-- Ex 18: You'll find a Color datatype below. It has the three basic
+-- colours Red, Green and Blue, and two color transformations, Mix and
+-- Darken.
+--
+-- Mix means the sum of the two colors.
+--
+-- Darken means darkening the color. The Double value tells how much
+-- the color is darkened. 0.0 means no change and 1.0 means full
+-- darkening, i.e. black.
+--
+-- Implement the function rgb :: Color -> [Double] that returns a list
+-- of length three that represents the rgb value of the given color.
+--
+-- Examples:
+--
+-- rgb Red   ==> [1,0,0]
+-- rgb Green ==> [0,1,0]
+-- rgb Blue  ==> [0,0,1]
+--
+-- rgb (Mix Red Green)                ==> [1,1,0]
+-- rgb (Mix Red (Mix Red Green))      ==> [1,1,0]
+-- rgb (Darken 0.2 Red)               ==> [0.8,0,0]
+-- rgb (Darken 0.2 (Darken 0.2 Red))  ==> [0.64,0,0]
+-- rgb (Mix (Darken 0.4 Red) (Darken 0.4 Red)) ==> [1,0,0]
+-- rgb (Mix (Darken 0.6 Red) (Darken 0.6 Red)) ==> [0.8,0,0]
+--
+-- NB! Mix should saturate at 1.0
+
+data Color = Red | Green | Blue | Mix Color Color | Darken Double Color
+  deriving Show
+
+rgb :: Color -> [Double]
+rgb col = undefined
---
+++ W3Test.hs
@@ -0,0 +1,286 @@
+module W3Test where
+
+import W3
+import Impl.Test
+
+import Data.Either
+import Control.Monad
+import Test.QuickCheck hiding (Result,reason,classify,(===))
+
+main = testExs tests
+
+tests = [[property $ ex1_safeDiv_zero, property $ ex1_safeDiv_nonZero]
+        ,[property $ ex2_eitherDiv_zero
+         ,property $ ex2_eitherDiv_nonZero]
+        ,[ex3_addEithers_both, ex3_addEithers_one, ex3_addEithers_neither]
+        ,[property $ ex4_mapMaybe_1, property $ ex4_mapMaybe_2]
+        ,[ex5_fred, ex5_age, ex5_name]
+        ,[ex6_TwoCounters]
+        ,[ex6_TwoCounters]
+        ,[ex8_UpDown]
+        ,[ex8_UpDown]
+        ,[ex10_valAtRoot_Nothing, ex10_valAtRoot_Just]
+        ,[ex11_treeSize]
+        ,[ex12_leftest_Nothing, ex12_leftest_Just]
+        ,[ex13_mapTree]
+        ,[ex14_insertL]
+        ,[ex15_measure]
+        ,[property $ ex16_mysum, property $ ex16_mylength]
+        ,[ex17_treeLeaves, ex17_treeSum]
+        ,[ex18_rgb_red
+         ,ex18_rgb_green
+         ,ex18_rgb_blue
+         ,ex18_rgb_darken
+         ,ex18_rgb_mix
+         ,ex18_rgb_complicated
+         ]
+        ]
+
+-- -- -- -- -- -- -- --
+
+ex1_safeDiv_zero x =
+  safeDiv x 0 === Nothing
+
+ex1_safeDiv_nonZero x y =
+  (y/=0) ==> safeDiv x y === Just (div x y)
+
+ex2_eitherDiv_zero x =
+  eitherDiv x 0 === Left (show x++"/0")
+
+ex2_eitherDiv_nonZero x y =
+  (y/=0) ==> eitherDiv x y === Right (div x y)
+
+ex3_addEithers_both = property $ do
+  x <- arbitrary
+  y <- arbitrary
+  return $ counterexample ("addEithers (Right "++show x++") (Right "++show y++")") $
+    addEithers (Right x) (Right y) === Right (x+y)
+
+ex3_addEithers_one = property $ do
+  err <- fmap Left arbitrary
+  int <- fmap Right arbitrary
+  [x,y] <- shuffle [err,int]
+  return $ counterexample ("addEithers ("++show x++") ("++show y++")") $
+    addEithers x y === err
+
+ex3_addEithers_neither = property $ do
+  x <- fmap Left arbitrary
+  y <- fmap Left arbitrary
+  return $ counterexample ("addEithers ("++show x++") ("++show y++")") $
+    addEithers x y === x
+
+ex4_mapMaybe_1 xs =
+  counterexample ("let f True = Just True; f False = Nothing in mapMaybe f "++show xs) $
+  mapMaybe f xs === filter id xs
+  where f True = Just True
+        f False = Nothing
+
+ex4_mapMaybe_2 :: [Integer] -> Property
+ex4_mapMaybe_2 is =
+  counterexample ("let f x = if x>0 then Just (2*x) else Nothing\
+                 \in mapMaybe f "++show is) $
+  mapMaybe f is === map (2*) (filter (>0) is)
+  where f x = if x>0 then Just (2*x) else Nothing
+
+ex5_fred = property $ do
+  conjoin [counterexample "getName fred" $
+           getName fred === "Fred"
+          ,counterexample "getAge fred" $
+           getAge fred === 90]
+
+word = listOf1 (choose ('a','z'))
+
+ex5_name = property $ do
+  n <- word
+  return $ counterexample ("getName (setName "++show n++" fred)") $
+    getName (setName n fred) === n
+
+ex5_age = property $ do
+  a <- choose (0,89)
+  return $ counterexample ("getAge (setAge "++show a++" fred)") $
+    getAge (setAge a fred) === a
+
+ex6_TwoCounters = property $ do
+  a <- choose (0,20)
+  b' <- choose (0,20)
+  let b = a+b'
+  let tc0 = iterate (incA . incB) zeros !! a
+      tc1 = iterate incB tc0 !! b'
+  return $ counterexample ("Did "++show a++" incAs and "++show b++" incBs.") $
+    (getA tc1, getB tc1) === (a,b)
+
+ex8_UpDown = property $ do
+  a <- choose (0,20)
+  b <- choose (0,20)
+  let tc0 = iterate tick zero !! a
+      tc1 = iterate tick (toggle tc0) !! b
+  return $ counterexample ("Did "++show a++" ticks, a toggle, and "++show b++" ticks.") $
+    get tc1 === a-b
+
+ex10_valAtRoot_Nothing =
+  valAtRoot Leaf === (Nothing :: Maybe Bool)
+
+ex10_valAtRoot_Just = property $ do
+  l <- genTree 3 :: Gen (Tree Integer)
+  r <- genTree 3 :: Gen (Tree Integer)
+  v <- choose (0,10 :: Integer)
+  let t = Node v l r
+  return $ counterexample (show t) $
+    valAtRoot t === Just v
+
+genTree :: Arbitrary a => Int -> Gen (Tree a)
+genTree 0 = return Leaf
+genTree siz = do
+  let siz' = siz-1
+  sizl <- choose (0,siz')
+  let sizr = siz'-sizl
+  l <- genTree sizl
+  r <- genTree sizr
+  v <- arbitrary
+  return $ Node v l r
+
+ex11_treeSize =
+  forAllShrink (choose (0,50)) shrink $ \s -> do
+    t <- genTree s
+    return $ counterexample (show t) $
+      treeSize (t :: Tree Int) === s
+
+genLeft :: Arbitrary a => a -> Int -> Gen (Tree a)
+genLeft k s = go s
+  where go 0 = do t <- genTree s
+                  return $ Node k Leaf t
+        go n = do r <- genTree s
+                  l <- go (n-1)
+                  v <- arbitrary
+                  return $ Node v l r
+
+ex12_leftest_Nothing = leftest Leaf === (Nothing :: Maybe Bool)
+
+ex12_leftest_Just = property $ do
+  k <- choose (0,10)
+  s <- choose (0,10)
+  t <- genLeft k s
+  return $ counterexample (show t) $
+    leftest (t :: Tree Int) === Just k
+
+genL :: Int -> Gen (Tree Bool -> Tree Bool)
+genL s = go s
+  where go 0 = do return $ id
+        go n = do r <- genTree s
+                  l <- go (n-1)
+                  v <- arbitrary
+                  return $ \k -> Node v (l k) r
+
+ex13_mapTree =
+  forAllShrink (choose (0,50)) shrink $ \s -> do
+    t <- genTree s
+    let t' = mapTree (even::Int->Bool) t
+    return $ counterexample ("mapTree even "++show t++"\returned:\n"++show t') $
+      check t t'
+  where check Leaf Leaf = property $ True
+        check (Node a al ar) bt@(Node b bl br) =
+          counterexample ("Alipuussa "++show bt) $
+          conjoin [b === even a,
+                   check al bl,
+                   check ar br]
+        check a b =
+          counterexample ("Tree structures don't match:\n"++show a++"\n"++show b) False
+
+ex14_insertL =
+  forAllShrink (choose (0,20)) shrink $ \s -> do
+    f <- genL s
+    let t0 = f Leaf
+        t1 = f (Node True Leaf Leaf)
+    return $ counterexample ("insertL True "++show t0) $
+      insertL True t0 === t1
+
+genMeasure 0 = return $ Leaf
+genMeasure siz = do
+  let siz' = siz-1
+  sizl <- choose (0,siz')
+  let sizr = siz'-sizl
+  l <- genMeasure sizl
+  r <- genMeasure sizr
+  return $ Node siz l r
+
+zeroTree Leaf = Leaf
+zeroTree (Node _ l r) = Node 0 (zeroTree l) (zeroTree r)
+
+ex15_measure =
+  forAllShrink (choose (0,20)) shrink $ \s -> do
+    t <- genMeasure s
+    let t' = zeroTree t :: Tree Int
+    return $ counterexample (show t') $
+      measure t' === t
+
+ex16_mysum :: [Int] -> Property
+ex16_mysum xs =
+  foldr sumf 0 xs === sum xs
+
+ex16_mylength :: [Int] -> Property
+ex16_mylength xs =
+  foldr lengthf 0 xs === length xs
+
+ex17_treeLeaves =
+  forAllShrink (choose (0,20)) shrink $ \s -> do
+    t <- genTree s
+    let leaves = s+1
+    return $ counterexample (show t) $
+      foldTree leaft 1 (t :: Tree Bool) === leaves
+
+modTree k Leaf = Leaf
+modTree k (Node _ l r) = Node k (modTree k l) (modTree k r)
+
+ex17_treeSum = property $ do
+  k <- choose (0,5 :: Int)
+  s <- choose (0,5)
+  t0 <- genTree s :: Gen (Tree ())
+  let t = modTree k t0
+  return $ counterexample (show t) $
+    foldTree sumt 0 t === s*k
+
+ex18_rgb_red =
+  counterexample (show Red) $
+  rgb Red === [1,0,0]
+ex18_rgb_green =
+  counterexample (show Green) $
+  rgb Green === [0,1,0]
+ex18_rgb_blue =
+  counterexample (show Blue) $
+  rgb Blue === [0,0,1]
+
+fcmp actual expected =
+  counterexample ("Expected " ++ show expected ++ ", got " ++ show actual) $
+    diff < eps
+  where diff = sum . map abs $ zipWith (-) actual expected
+        eps = 0.01
+
+ex18_rgb_darken = property $ do
+  s <- choose (0,1)
+  let col = Darken s (Darken s Red)
+  let ans = rgb col
+  return $ counterexample (show col) $
+    fcmp ans [(1-s)^2, 0, 0]
+
+ex18_rgb_mix = property $ do
+  r <- choose (0,1)
+  g <- choose (0,1)
+  let col = Mix (Darken r Red) (Darken g Green)
+  let ans = rgb col
+  return $ counterexample (show col) $
+    fcmp ans [(1-r), (1-g), 0]
+
+ex18_rgb_complicated = property $ do
+  [r0,b0,g0,r1,b1,g1] <- replicateM 6 (choose (0,1))
+  [x,y] <- replicateM 2 (choose (0,0.1))
+  let c0 = Darken x (Mix (Darken (1-r0) Red) (Mix (Darken (1-b0) Blue) (Darken (1-g0) Green)))
+      c1 = Mix (Darken (1-g1) Green) (Mix (Darken (1-b1) Blue) (Darken (1-r1) Red))
+      c = Darken y (Mix c0 c1)
+      ans = rgb c
+      f = min 1
+      x' = 1-x
+      y' = 1-y
+  return $ counterexample (show c) $
+    fcmp ans [y'*(f $ x'*r0+r1),
+              y'*(f $ x'*g0+g1),
+              y'*(f $ x'*b0+b1)]
