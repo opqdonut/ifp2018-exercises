51958af8f83efe9fcb722b205acf221dbe9cb659
Joel Kaasinen <joel.kaasinen@gmail.com>
2018-04-18 19:33:23 +0300

    W4: make tests more robust
    at least in one case using stop caused an ambiguous type error, which
    return solved

--- W4Test.hs
+++ W4Test.hs
@@ -39,44 +39,44 @@ tests = [[ex1_hello]
 
 ex1_hello = monadicIO $ do
   (text,()) <- runc' hello
-  stop $ text === "HELLO\nWORLD\n"
+  return $ text === "HELLO\nWORLD\n"
 
 word = listOf1 (choose ('a','z'))
 
 ex2_greet = monadicIO $ do
   name <- pick word
   (text,()) <- runc' $ greet name
-  stop $ text === ("HELLO "++name++"\n")
+  return $ text === ("HELLO "++name++"\n")
 
 ex3_greet2 = monadicIO $ do
   name <- pick word
   (text,()) <- runc (name++"\n") greet2
-  stop $ text === ("HELLO "++name++"\n")
+  return $ text === ("HELLO "++name++"\n")
 
 ex4_getSum = monadicIO $ do
   a <- pick (arbitrary :: Gen Int)
   b <- pick (arbitrary :: Gen Int)
   (_,ret) <- runc (show a++"\n"++show b++"\n") getSum
-  stop $ ret === a+b
+  return $ ret === a+b
 
 ex5_readWords = monadicIO $ do
   words <- pick $ listOf1 word
   (_,ret) <- runc (unlines words) (readWords (length words - 1))
-  stop $ ret === sort (init words)
+  return $ ret === sort (init words)
 
 ex6_readUntil = monadicIO $ do
   end <- pick word
   words <- pick $ listOf1 (word `suchThat` (/=end))
   let input = unlines $ words ++ [end]
   (_,ret) <- runc input (readUntil (==end))
-  stop . counterexample ("readUntil (==" ++ show end ++ ")\nInput: "++show input) $
+  return . counterexample ("readUntil (==" ++ show end ++ ")\nInput: "++show input) $
     ret === words
 
 ex7_isums = monadicIO $ do
   numbers <- pick . listOf1 $ choose (-10,10)
   let n = length numbers
   (text,ret) <- runc (unlines $ map show numbers) $ isums n
-  stop . counterexample ("isums "++show n) $
+  return . counterexample ("isums "++show n) $
     conjoin [counterexample "returning" $
              ret === sum numbers,
              counterexample "printing" $
@@ -88,7 +88,7 @@ ex8_whenM_True = monadicIO $ do
   let cond = return True
   run $ whenM cond op
   v <- run $ readIORef r
-  stop $ counterexample "whenM (return True)" $
+  return $ counterexample "whenM (return True)" $
     v
 
 ex8_whenM_False = monadicIO $ do
@@ -97,7 +97,7 @@ ex8_whenM_False = monadicIO $ do
   let cond = return False
   run $ whenM cond op
   v <- run $ readIORef r
-  stop $ counterexample "whenM (return False)" $
+  return $ counterexample "whenM (return False)" $
     not v
 
 ex9_while = monadicIO $ do
@@ -109,7 +109,7 @@ ex9_while = monadicIO $ do
   run $ while ehto op
   af <- run $ readIORef a
   bf <- run $ readIORef b
-  stop $ counterexample "while" $
+  return $ counterexample "while" $
     conjoin [counterexample "number of calls to condition" $ af === i+1,
              counterexample "number of calls to operation" $ bf === i]
 
@@ -118,7 +118,7 @@ ex10_debug = monadicIO $ do
   value <- pick word
   print <- pick word
   (text,ret) <- runc' $ debug token (putStrLn print >> return value)
-  stop $ counterexample ("debug "++show token++" (do putStrLn "++show print++"; return "++show value++")") $
+  return $ counterexample ("debug "++show token++" (do putStrLn "++show print++"; return "++show value++")") $
     conjoin [counterexample "tulostus" $ text === (token ++ "\n" ++ print ++ "\n" ++ token ++ "\n"),
              counterexample "palautus" $ ret === value]
 
@@ -128,7 +128,7 @@ ex11_mapM_ = monadicIO $ do
   let op x = modifyIORef r (x:)
   run $ mymapM_ op lis
   ret <- run $ readIORef r
-  stop $ counterexample ("mapM op "++show lis) $
+  return $ counterexample ("mapM op "++show lis) $
     ret === reverse lis
 
 ex12_forM = monadicIO $ do
@@ -138,7 +138,7 @@ ex12_forM = monadicIO $ do
                 return $ x+1
   ret <- run $ myforM lis op
   out <- run $ readIORef r
-  stop $ counterexample ("forM "++show lis++" op") $
+  return $ counterexample ("forM "++show lis++" op") $
     conjoin [counterexample "return value" $ ret === map (+1) lis,
              counterexample "side effects" $ out === reverse lis]
 
@@ -146,7 +146,7 @@ ex13_doubleCall = monadicIO $ do
   i <- pick $ (choose (0,20) :: Gen Int)
   let op = return (return i)
   out <- run $ doubleCall $ op
-  stop $ counterexample ("doubleCall (return (return "++show i++"))") $
+  return $ counterexample ("doubleCall (return (return "++show i++"))") $
     out === i
 
 ex14_compose = monadicIO $ do
@@ -154,7 +154,7 @@ ex14_compose = monadicIO $ do
   let op1 = return . (*2)
       op2 = return . (+1)
   out <- run $ compose op1 op2 i
-  stop $ counterexample "compose (return . (*2)) (return . (+1))" $
+  return $ counterexample "compose (return . (*2)) (return . (+1))" $
     out === (i+1)*2
 
 ex15_mkCounter = monadicIO $ do
@@ -162,7 +162,7 @@ ex15_mkCounter = monadicIO $ do
   m <- run $ do (i,g) <- mkCounter
                 replicateM_ n i
                 g
-  stop $ m === n
+  return $ m === n
 
 ex16_hFetchLines = monadicIO $ do
   lines <- pick $ listOf1 word
@@ -175,7 +175,7 @@ ex16_hFetchLines = monadicIO $ do
 
   outs <- run $ hFetchLines h inds
 
-  stop $ counterexample ("hFetchLines h "++show inds++"\nContents:\n"++unlines lines) $
+  return $ counterexample ("hFetchLines h "++show inds++"\nContents:\n"++unlines lines) $
     outs === map ((lines!!).pred) inds
 
 toCSV = unlines . map (intercalate ",")
@@ -192,7 +192,7 @@ ex17_readCSV = monadicIO $ do
   let dat' = toCSV dat
   path <- run $ tmpSpit "readCSV.in" dat'
   ret <- run $ readCSV path
-  stop $ counterexample ("File contents: "++show dat') $ ret === dat
+  return $ counterexample ("File contents: "++show dat') $ ret === dat
 
 ex18_compareFiles = monadicIO $ do
   alines <- pick $ listOf1 word
@@ -206,7 +206,7 @@ ex18_compareFiles = monadicIO $ do
   path2 <- run $ tmpSpit "compareFilesB.in" bc
   (outp,()) <- runc' $ compareFiles path1 path2
   let ls = lines outp
-  stop $ counterexample ("compareFiles\nFile A:\n"++ac++"File B:\n"++bc) $
+  return $ counterexample ("compareFiles\nFile A:\n"++ac++"File B:\n"++bc) $
     conjoin [counterexample "number of lines printed" $ length ls === 2*length diffs,
              counterexample "lines printed" $ ls === should]
 
@@ -215,7 +215,7 @@ ex19_interact_terminates = monadicIO $ do
       f (s,_) = (False,s,s)
   w <- pick $ word
   (text,ret) <- runc w $ interact' f ""
-  stop $ conjoin [counterexample "tulostus" $ text === w,
+  return $ conjoin [counterexample "tulostus" $ text === w,
                   counterexample "palautus" $ ret === w]
 
 ex19_interact_loop = monadicIO $ do
@@ -226,5 +226,5 @@ ex19_interact_loop = monadicIO $ do
       eret = reverse $ 0:is
       etext = unlines $ replicate (length is) "PICK" ++ ["END"]
   (text,ret) <- runc (unlines $ map show is ++ ["END"]) $ interact' f [0]
-  stop $ conjoin [counterexample "printing" $ text === etext,
+  return $ conjoin [counterexample "printing" $ text === etext,
                   counterexample "return value" $ ret === eret]
